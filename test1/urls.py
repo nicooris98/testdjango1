41c8ee8112a3a6a35588685e9b63e202da461a7f
nicolasoris98 <nicolasoris98@gmail.com>
2020-10-12 01:04:28 -0300

    se agregaron las plantillas

--- test1/urls.py
+++ test1/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from test1.views import saludo
+from test1.views import saludo, despedida, dameFecha, calculaEdad
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('nosvemos/', despedida),
+    path('fecha/', dameFecha),
+    path('edades/<int:edad>/<int:ano>', calculaEdad)
 ]
--- test1/views.py
+++ test1/views.py
@@ -1,5 +1,43 @@
 from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+
+ruta="E:/NICO/Informatica/Django/test1/test1/"
 
 def saludo(request):#primera vista
+    #documento="<html><body><h1>Primera Pagina Django</h1></body></html>"
+    doc_externo=open(ruta+"plantillas/plantilla1.html")
+    plt=Template(doc_externo.read())
+    doc_externo.close()
+    ctx=Context()
+    documento=plt.render(ctx)
+    return HttpResponse(documento)
+
+def despedida(request):
+    return HttpResponse("Adios")
+
+def dameFecha(request):
+    fecha_actual=datetime.datetime.now()
+    documento="""
+    <html>
+    <body>
+    <h2>
+    Fecha y Hora actuales %s
+    </h2>
+    </body>
+    </html>""" % fecha_actual
+    return HttpResponse(documento)
+
+def calculaEdad(request, edad, ano):
+    #edadActual=22
+    periodo=ano-2020
+    edadFutura=edad+periodo
+    documento="""<html>
+    <body>
+    <h2>
+    En el año %s tendras %s años
+    </h2>
+    </body>
+    </html>""" %(ano, edadFutura)
+    return HttpResponse(documento)
     
-    return HttpResponse("Primera pagina Django")
